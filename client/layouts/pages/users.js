a385d1696c39764331cb2b3bfb68482a204cda8e
Grazzhopper <danny.otter@gmail.com>
2016-07-18 22:06:17 +0000

    added user updates through server method

--- client/layouts/pages/users.js
+++ client/layouts/pages/users.js
@@ -1,11 +1,44 @@
 Template.Users.onCreated(function() {
   this.autorun(() => {
     this.subscribe('allUsers');
+    this.subscribe('allRoles');
   });
 });
 
 Template.Users.helpers({
   users: function() {
     return Meteor.users.find();
+  },
+  userRole: function(){
+    return this.roles[0];
+  },
+  dateFormat: function() {
+    return moment(this.createdAt).format('D MMMM YYYY, hh:mm:ss')
+  },
+  selectedUser: function() {
+    return Meteor.users.findOne(Session.get('selecteduserdata'));
+  }
+  
+});
+
+Template.Users.events({
+
+  'click .edit-user': function() {
+    Session.set('selecteduserdata', this._id);
+    Session.set('edittoggle', true);
+  },
+
+  'submit form': function(e) {
+    e.preventDefault();
+    var currentUserId = Session.get('selecteduserdata');
+    const targetCompany = e.company;
+    console.log (e.target.company.value);
+    var postProperties = {
+        company: e.target.company.value,
+        role: e.target.role.value
+      };
+      
+    Meteor.call('updateUser', currentUserId, postProperties);
+
   }
 });
